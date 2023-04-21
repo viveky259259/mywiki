2c9c0adcde1366ff89fe24673ef4afe013baec7f
viveky259259 <viveky259259@gmail.com>
2023-04-21 22:44:04 +0530

    1. Added MyWiki App 2. added MVC+ clean architecture for search page

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mywiki/src/feature/search/view/search_page.dart';
+
+class MyWikiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'My Wiki',
+      home: SearchPage(),
+    );
+  }
+}
