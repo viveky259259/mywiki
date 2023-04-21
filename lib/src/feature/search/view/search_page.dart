0576263fd2d983ed9dfc1b832a6e01482d396027
viveky259259 <viveky259259@gmail.com>
2023-04-21 22:59:35 +0530

    Added placeholder for bloc in search page

--- lib/src/feature/search/view/search_page.dart
+++ lib/src/feature/search/view/search_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mywiki/src/feature/search/bloc/search_bloc.dart';
+import 'package:mywiki/src/feature/search/bloc/search_event.dart';
+import 'package:mywiki/src/feature/search/bloc/search_state.dart';
 
 class SearchPage extends StatelessWidget {
-  const SearchPage({super.key});
-
+  SearchPage({super.key});
+  final SearchBloc _searchBloc = SearchBloc();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: BlocBuilder<SearchBloc, SearchState>(
+          bloc: _searchBloc,
+          builder: (context, state) {
+            if (state is InitialState) {
+              return ElevatedButton(
+                  onPressed: () {
+                    _searchBloc.add(SearchUserInputEvent('Vivek'));
+                  },
+                  child: Text('Search Vivek'));
+            } else if (state is LoadingCachedResult) {
+              return CircularProgressIndicator();
+            } else if (state is LoadingSearchResult) {
+              return CircularProgressIndicator();
+            } else if (state is LoadedSearchResult) {
+              return Text('WIP');
+            } else {
+              return Text('something went wrong');
+            }
+          }),
+    );
   }
 }
